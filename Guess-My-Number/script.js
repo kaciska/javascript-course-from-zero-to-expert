1b3eb001db15a51f73b8d1122b0c38a9654aa254
blatnak <katerina.blatna@uzis.cz>
2022-09-23 15:31:53 +0200

    basic game logic in js added

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -1,2 +1,36 @@
-'use strict';
+const check = document.querySelector('.check')
+const message = document.querySelector('.message')
+const score = document.querySelector('.score')
+let scoreNumber = 20 // state variable
 
+//Math.trunc()removes the decimals (does NOT round the number)
+const secretNumber = Math.trunc(Math.random() * 20) + 1
+document.querySelector('.number').textContent = secretNumber
+
+check.addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value) // default is string, we need number
+
+  if (!guess) {
+    message.textContent = 'Missing number!'
+  } else if (guess === secretNumber) {
+    message.textContent = 'Correct number!'
+  } else if (guess > secretNumber) {
+    if (scoreNumber > 1) {
+      message.textContent = 'Too high!'
+      scoreNumber--
+      score.textContent = scoreNumber
+    } else {
+        message.textContent = "You lost the game!"
+        score.textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (scoreNumber > 1) {
+      message.textContent = 'Too low!'
+      scoreNumber--
+      score.textContent = scoreNumber
+    } else {
+        message.textContent = "You lost the game!"
+        score.textContent = 0;
+    }
+  }
+})
