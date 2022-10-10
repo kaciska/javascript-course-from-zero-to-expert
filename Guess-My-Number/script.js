0ec4628bb9175ce634da8a65dd295d33fcee5ad2
blatnak <katerina.blatna@uzis.cz>
2022-10-10 14:20:34 +0200

    Refactoring, DRY principle

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -1,6 +1,5 @@
 const check = document.querySelector('.check')
 const replay = document.querySelector('.again')
-const message = document.querySelector('.message')
 const score = document.querySelector('.score')
 let number = document.querySelector('.number')
 let scoreNumber = 20 // state variable
@@ -8,45 +7,40 @@ let highscoreNumber = 0 // state variable
 
 //Math.trunc()removes the decimals (does NOT round the number)
 const secretNumber = Math.trunc(Math.random() * 20) + 1
-//number.textContent = secretNumber
 
 check.addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value) // default is string, we need number
-  
+
+  const displayMessage = function (message) {
+    // function with parameter (message)
+    document.querySelector('.message').textContent = message
+  }
 
   // no input
   if (!guess) {
-    message.textContent = 'Missing number!'
+    displayMessage('Missing number!')
 
-    // player wins
+  // player wins
   } else if (guess === secretNumber) {
-    message.textContent = 'Correct number!' 
+    displayMessage('Correct number!')
+
     number.textContent = secretNumber
     document.querySelector('body').style.backgroundColor = '#60b347'
     number.style.width = '30rem'
 
     // check highscore
-    if (scoreNumber  > highscoreNumber) {    
-      highscoreNumber = scoreNumber  
-     
-      localStorage.setItem("highscoreNumber", highscoreNumber);
-      document.querySelector('.highscore').innerHTML = localStorage.getItem("highscoreNumber")      
-    }    
+    if (scoreNumber > highscoreNumber) {
+      highscoreNumber = scoreNumber
 
-    // guess is too high
-  } else if (guess > secretNumber) {
-    if (scoreNumber > 1) {
-      message.textContent = 'Too high!'
-      scoreNumber--
-      score.textContent = scoreNumber
-    } else {
-      message.textContent = 'You lost the game!'
-      score.textContent = 0
+      localStorage.setItem('highscoreNumber', highscoreNumber)
+      document.querySelector('.highscore').innerHTML =
+        localStorage.getItem('highscoreNumber')
     }
-    // guess is too low
-  } else if (guess < secretNumber) {
+
+  // guess is too high OR too low
+  } else if (guess !== secretNumber) {
     if (scoreNumber > 1) {
-      message.textContent = 'Too low!'
+      displayMessage(guess > secretNumber ? 'Too high!' : 'Too low!')
       scoreNumber--
       score.textContent = scoreNumber
     } else {
@@ -58,13 +52,14 @@ check.addEventListener('click', function () {
 
 // replay the game
 replay.addEventListener('click', function () {
-  window.location.reload()  
+  window.location.reload()
 })
 
 // set highscore
-window.onload = function() {
-  document.querySelector('.highscore').innerHTML = window.localStorage.getItem('highscoreNumber');
-}  
+window.onload = function () {
+  document.querySelector('.highscore').innerHTML =
+    window.localStorage.getItem('highscoreNumber')
+}
 
 // Clear localstorage
 // localStorage.clear();
