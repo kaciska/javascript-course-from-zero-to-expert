37148e60575c0966d78670671ef082777440f206
blatnak <katerina.blatna@uzis.cz>
2022-10-10 16:23:16 +0200

    eventListeners for btn click & keydown

--- Modal-Window/script.js
+++ Modal-Window/script.js
@@ -1,10 +1,31 @@
-'use strict';
+'use strict'
 
-const modal = document.querySelector(".modal")
-const overlay = document.querySelector(".overlay")
-const btnCloseModal = document.querySelector(".close-modal")
-const btnsShowModal = document.querySelectorAll(".show-modal")
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const btnCloseModal = document.querySelector('.close-modal')
+const btnsShowModal = document.querySelectorAll('.show-modal')
+
+const closeModal = () => {
+  modal.classList.add('hidden') // classes here without dot
+  overlay.classList.add('hidden')
+}
+
+const showModal = () => {
+  modal.classList.remove('hidden') 
+  overlay.classList.remove('hidden')
+}
 
 btnsShowModal.forEach((btn) => {
-    console.log(btn.textContent)
+  btn.addEventListener('click', showModal)
+})
+
+btnCloseModal.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    console.log('test')
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+  }
 })
