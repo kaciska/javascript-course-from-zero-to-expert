ef080b32990d1034a429eff74f5696679e79d823
blatnak <katerina.blatna@uzis.cz>
2022-11-03 12:19:23 +0100

    Naming values conventions

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,6 +1,22 @@
 let js = 'amazing';
-
-if (js === 'amazing') alert('Javascript is FUN');
-
-42 + 18 - 7 + 1;
 console.log(42 + 18 - 7 + 1);
+
+// use camelCase 
+let firstName = "Katka";
+
+// different color of the variable name because of the capital letters
+const PI = 3.1415; 
+
+// names can only contain numbers, letters, underscores or $ sign
+// dont use names like: name, function, new -> they are reserved JS keyboard
+
+// dont use first capital letter f.e. Person but
+let person = "Tomas";
+
+// use descriptive variable names
+let myFirstJob = "Marketing Coordinator";
+let myCurrentJob = "Programmer";
+
+// insted of this
+let job1 = "Marketing Coordinator";
+let job2 = "Programmer";
