30d94cf0990714b79b8a868949872354ea392f4f
blatnak <katerina.blatna@uzis.cz>
2022-11-04 10:56:38 +0100

    Basic operators exercising

--- 01-Fundamentals-Part-1/assigments.js
+++ 01-Fundamentals-Part-1/assigments.js
@@ -24,6 +24,7 @@ to the console
 
 const isIsland = false;
 //const language; it cannot be empty, if it is const
+const language = "Czech";
 
 console.log(typeof isIsland);
 console.log(typeof population);
@@ -39,4 +40,32 @@ change, and which might change?). Then, change these variables to const.
 3. Try to change one of the changed variables now, and observe what happens
 */
 
-const language = "Czech";
+// const language = "Czech"; already declared
+
+/*
+LECTURE: Basic Operators
+1. If your country split in half, and each half would contain half the population,
+then how many people would live in each half?
+2. Increase the population of your country by 1 and log the result to the console
+3. Finland has a population of 6 million. Does your country have more people than
+Finland?
+4. The average population of a country is 33 million people. Does your country
+have less people than the average country?
+5. Based on the variables you created, create a new variable 'description'
+which contains a string with this format: 'Portugal is in Europe, and its 11 million
+people speak portuguese'
+ */
+
+let halfCountryPopulation = population / 2;
+console.log("Half population of Czech republic is", halfCountryPopulation, "millions.")
+
+console.log(population += 1);
+
+const hasMorePopulation = population > 6;
+console.log("It is", hasMorePopulation, "that Finland has bigger population than Czech republic.")
+
+const moreThanAveragePopulation = population < 33;
+console.log("It is", moreThanAveragePopulation, "that Czech republic has less people than average country.")
+
+const description = `${country} is in ${continent}, and its ${population} million people speak ${language}.`
+console.log(description);
