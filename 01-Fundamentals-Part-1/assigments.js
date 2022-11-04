12ce4649720c0417169e57fed7460bab9e0e1ca9
blatnak <katerina.blatna@uzis.cz>
2022-11-04 10:42:53 +0100

    Declaring variables changes

--- 01-Fundamentals-Part-1/assigments.js
+++ 01-Fundamentals-Part-1/assigments.js
@@ -7,13 +7,14 @@ assign their values according to your own country (population in millions)
 2. Log their values to the console
 */
 
-let country = "Czech republic";
-let continent = "Europe";
+const country = "Czech republic";
+const continent = "Europe";
 let population = 10;
 
 console.log("I live in the " + country + " in the " + continent + "." + " Our country has " + population + " millions inhabitants.");
 
-/* LECTURE: Data Types 
+/* 
+LECTURE: Data Types 
 1. Declare a variable called 'isIsland' and set its value according to your
 country. The variable should hold a Boolean value. Also declare a variable
 'language', but don't assign it any value yet
@@ -21,10 +22,21 @@ country. The variable should hold a Boolean value. Also declare a variable
 to the console
 */
 
-let isIsland = false;
-let language;
+const isIsland = false;
+//const language; it cannot be empty, if it is const
 
 console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
+
+/*
+LECTURE: let, const and var
+1. Set the value of 'language' to the language spoken where you live (some
+countries have multiple languages, but just choose one)
+2. Think about which variables should be const variables (which values will never
+change, and which might change?). Then, change these variables to const.
+3. Try to change one of the changed variables now, and observe what happens
+*/
+
+const language = "Czech";
