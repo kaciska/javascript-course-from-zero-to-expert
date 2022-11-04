f0b0bf1cd577d0b97e44788bb8cdae54b2c01704
blatnak <katerina.blatna@uzis.cz>
2022-11-04 13:29:09 +0100

    BMI coding challenges done

--- 01-Fundamentals-Part-1/assigments.js
+++ 01-Fundamentals-Part-1/assigments.js
@@ -54,7 +54,7 @@ have less people than the average country?
 5. Based on the variables you created, create a new variable 'description'
 which contains a string with this format: 'Portugal is in Europe, and its 11 million
 people speak portuguese'
- */
+*/
 
 let halfCountryPopulation = population / 2;
 console.log("Half population of Czech republic is", halfCountryPopulation, "millions.")
@@ -69,3 +69,59 @@ console.log("It is", moreThanAveragePopulation, "that Czech republic has less pe
 
 const description = `${country} is in ${continent}, and its ${population} million people speak ${language}.`
 console.log(description);
+
+/*Coding Challenge #1
+Mark and John are trying to compare their BMI (Body Mass Index), which is
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+and height in meter).
+Your tasks:
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
+Test data:
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+m tall.
+*/
+
+/*const heightMark = 1.69;
+const weightMark = 78;
+const heightJohn = 1.95;
+const weightJohn = 92;*/
+
+// const used because I already know that I'm not going to change it here
+const heightMark = 1.88;
+const weightMark = 95;
+const heightJohn = 1.76;
+const weightJohn = 85;
+
+const bmiMark =  (weightMark / heightMark ** 2).toFixed(1);
+console.log("BMI Mark", bmiMark);
+
+const bmiJohn =  (weightJohn / heightJohn ** 2).toFixed(1);
+console.log("BMI John", bmiJohn);
+
+const markHigherBMI = bmiMark > bmiJohn;
+console.log("The statement, that Mark's BMI is higher than John's, is", markHigherBMI);
+
+
+/*
+Coding Challenge #2
+Use the BMI example from Challenge #1, and the code you already wrote, and
+improve it.
+Your tasks:
+1. Print a nice output to the console, saying who has the higher BMI. The message
+is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
+BMI (28.3) is higher than John's (23.9)!"
+ */
+
+if (bmiMark > bmiJohn) {
+    console.log(`Mark's BMI ${bmiMark} is higher than John's ${bmiJohn}!`)
+} else {
+    console.log(`John's BMI ${bmiJohn} is higher than Mark's ${bmiMark}!`)
+}
